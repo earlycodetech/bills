b786f8d7e81ffced566411bd88853504972192b4
Joseph Ogbu <fallyfox@gmail.com>
2024-10-09 12:53:28 +0100

    bare project

---
+++ app/about-us/page.js
@@ -0,0 +1,8 @@
+export default function AboutUs () {
+    return (
+        <div className="h-screen bg-gray-200 px-3 md:px-8 lg:px-16">
+            <h1>About Us</h1>
+            <p>We provide personal loans with easy payment plans</p>
+        </div>
+    )
+}
